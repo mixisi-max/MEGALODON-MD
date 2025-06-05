2abd57fb4f85309ac84592a75bfb632eb30e2254
mixisi-max <sory060916@gmail.com>
2025-06-05 01:40:28 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id here",
+SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~SZJi1T4J#kQLLUZ_UEpFzZY8oE3h7shkeK4_FTm2Q8OtJsvkLym8",
 // add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
@@ -20,7 +20,7 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MEGALODON-MD 🤍*",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
+WELCOME: process.env.WELCOME || "false",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
@@ -34,11 +34,31 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/frns4k.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "&",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃",
+BOT_NAME: process.env.BOT_NAME || "𓆩꧁𝕊𝔼ℂ𝕂 𝐓𝐇𝐄 𝐏𝐑𝐈𝐍𝐂𝐄𝐒𝐒𓆪꧂𓆪⁩   
+
+𐃁𝐓𒌧    
+𐃁𝐄́𒌧 
+𐃁𝐍𒌧
+𐃁𝐄́𒌧 
+𐃁𝐁𒌧 
+𐃁𝐑𒌧 
+𐃁𝐄𒌧  
+
+𐃁CsLC𒆙",
 // add bot name here for menu
-STICKER_NAME: process.env.STICKER_NAME || "put your name",
+STICKER_NAME: process.env.STICKER_NAME || "𓆩꧁𝕊𝔼ℂ𝕂 𝐓𝐇𝐄 𝐏𝐑𝐈𝐍𝐂𝐄𝐒𝐒𓆪꧂𓆪⁩   
+
+𐃁𝐓𒌧    
+𐃁𝐄́𒌧 
+𐃁𝐍𒌧
+𐃁𝐄́𒌧 
+𐃁𝐁𒌧 
+𐃁𝐑𒌧 
+𐃁𝐄𒌧  
+
+𐃁CsLC𒆙",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custom emoji react    
@@ -46,9 +66,19 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // choose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links without removing member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "224628204181",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ᴅʏʙʏ ᴛᴇᴄʜ",
+OWNER_NAME: process.env.OWNER_NAME || "𓆩꧁𝕊𝔼ℂ𝕂 𝐓𝐇𝐄 𝐏𝐑𝐈𝐍𝐂𝐄𝐒𝐒𓆪꧂𓆪⁩   
+
+𐃁𝐓𒌧    
+𐃁𝐄́𒌧 
+𐃁𝐍𒌧
+𐃁𝐄́𒌧 
+𐃁𝐁𒌧 
+𐃁𝐑𒌧 
+𐃁𝐄𒌧  
+
+𐃁CsLC𒆙",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴇɢᴀʟᴏᴅᴏɴ ᴍᴅ*",
 // bot description    
@@ -62,13 +92,13 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // public/private/inbox/group 
 ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
 // kick user if share link 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // auto voice messages
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 // auto stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // automatic text reply 
